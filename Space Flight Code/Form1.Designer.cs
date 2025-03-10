5119799862480be4c90bcf5fb7b569503d5bc4a8
Pakhomov Nikita <nikitapahomov97@gmail.com>
2025-03-10 10:17:01 +0300

    add zoomig alpha

--- Space Flight Code/Form1.Designer.cs
+++ Space Flight Code/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.resetbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -38,21 +42,59 @@
             this.panel1.Size = new System.Drawing.Size(716, 596);
             this.panel1.TabIndex = 0;
             // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 0;
+            this.trackBar1.Location = new System.Drawing.Point(93, 585);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(248, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(12, 585);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(75, 23);
+            this.startbtn.TabIndex = 4;
+            this.startbtn.Text = "Старт";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // resetbtn
+            // 
+            this.resetbtn.Location = new System.Drawing.Point(12, 556);
+            this.resetbtn.Name = "resetbtn";
+            this.resetbtn.Size = new System.Drawing.Size(75, 23);
+            this.resetbtn.TabIndex = 5;
+            this.resetbtn.Text = "Сброс";
+            this.resetbtn.UseVisualStyleBackColor = true;
+            this.resetbtn.Click += new System.EventHandler(this.resetbtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1171, 620);
+            this.Controls.Add(this.resetbtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Button resetbtn;
     }
 }
 
--- Space Flight Code/Form1.cs
+++ Space Flight Code/Form1.cs
@@ -17,16 +17,102 @@ using System.IO;
 using System.Windows;
 using System.Security.Policy;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Input;
 
 namespace Space_Flight_Code
 {
     public partial class Form1 : Form
     {
         private HelixViewport3D helixViewport;
+        private Earth earth;
+        private Moon moon;
+        private DispatcherTimer animationTimer;
+        private double earthRotationAngle;
+        private double moonOrbitAngle;
+        private double moonSelfRotationAngle;
+        private Object trackedObject;
+        private PerspectiveCamera defaultCamera;
+        private bool isTracking;
+
+
+
 
         public Form1()
         {
             InitializeComponent();
+            InitializeViewport();
+            InitializeObjects();
+            InitializeAnimation();
+            InitializeCameraTracking();
+        }
+
+        private void InitializeCameraTracking()
+        {
+            // Сохраняем начальные параметры камеры
+            defaultCamera = new PerspectiveCamera
+            {
+                Position = new Point3D(0, 500000, 1500000),
+                LookDirection = new Vector3D(0, -500000, -1500000),
+                UpDirection = new Vector3D(0, 1, 0),
+                FarPlaneDistance = 5000000
+            };
+
+            helixViewport.Camera = defaultCamera.Clone();
+            helixViewport.MouseDown += Viewport_MouseDown;
+        }
+
+        private void Viewport_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var hit = helixViewport.Viewport.FindNearestVisual(e.GetPosition(helixViewport.Viewport));
+            if (hit is ModelVisual3D visual && visual.Content != null)
+            {
+                var model = visual.Content as Model3DGroup;
+                if (model != null)
+                {
+                    trackedObject = FindObjectByModel(model);
+                    isTracking = true;
+                }
+            }
+        }
+
+        private Object FindObjectByModel(Model3DGroup model)
+        {
+            if (model == earth.Model) return earth;
+            if (model == moon.Model) return moon;
+            return null;
+        }
+
+        private void UpdateCameraTracking()
+        {
+            if (!isTracking || trackedObject == null) return;
+
+            var currentCamera = helixViewport.Camera as PerspectiveCamera;
+            if (currentCamera == null) return;
+
+            // Рассчитываем новую позицию камеры
+            var targetPosition = trackedObject.TransformGroup.Transform(trackedObject.Center);
+            var cameraOffset = new Vector3D(0, 0, trackedObject.Radius * 5);
+            var newPosition = targetPosition + cameraOffset;
+
+            // Плавное перемещение камеры
+            currentCamera.Position = Lerp(currentCamera.Position, newPosition, 0.5);
+            currentCamera.LookDirection = targetPosition - currentCamera.Position;
+            
+        }
+
+        private Point3D Lerp(Point3D a, Point3D b, double t)
+        {
+            return new Point3D(
+                a.X + (b.X - a.X) * t,
+                a.Y + (b.Y - a.Y) * t,
+                a.Z + (b.Z - a.Z) * t);
+        }
+
+
+        private void InitializeViewport()
+        {
 
 
             // Создание WPF-элемента
@@ -51,21 +137,85 @@ namespace Space_Flight_Code
             helixViewport.ShowFrameRate = true;        // Отключает FPS-счетчик
             helixViewport.ShowCoordinateSystem = false; // Отключает оси координат
             helixViewport.ShowCameraTarget = false;     // Отключает маркер центра вращения
-            helixViewport.Camera.LookAt();
+            
 
 
-            // Добавление сферы
-            Earth earth = new Earth();
-            Moon moon = new Moon();
-
-            helixViewport.Children.Add(new ModelVisual3D { Content = earth.Draw() });
-            helixViewport.Children.Add(new ModelVisual3D { Content = moon.Draw() });
+            //helixViewport.Camera.LookAt(earth.Center, 1);
 
 
-            helixViewport.ZoomExtents();
+            //helixViewport.ZoomExtents();
 
         }
 
+        private void InitializeObjects()
+        {
+            earth = new Earth();
+            moon = new Moon();
+
+            helixViewport.Children.Add(new ModelVisual3D { Content = earth.Draw() });
+            helixViewport.Children.Add(new ModelVisual3D { Content = moon.Draw() });
+        }
+
+        private void InitializeAnimation()
+        {
+            animationTimer = new DispatcherTimer();
+            animationTimer.Interval = TimeSpan.FromMilliseconds(16); // ~60 FPS
+            animationTimer.Tick += AnimationTimer_Tick;
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            const double earthRotationSpeed = 10; // Градусов/сек
+            const double moonOrbitSpeed = 10;      // Градусов/сек
+            const double moonRotationSpeed = 10;   // Градусов/сек 0.22 вроде относительно
+
+            earthRotationAngle += earthRotationSpeed * 0.001;
+            moonOrbitAngle += moonOrbitSpeed * 0.001;
+            moonSelfRotationAngle += moonRotationSpeed * 0.001;
+
+            earth.SelfRotation.Angle = earthRotationAngle % 360;
+
+            moon.SelfRotation.Angle = moonSelfRotationAngle % 360;
+
+            moon.UpdateOrbit(moonOrbitAngle);
+
+            UpdateCameraTracking();
+
+            helixViewport.InvalidateVisual();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            animationTimer.Interval = TimeSpan.FromMilliseconds((trackBar1.Maximum - trackBar1.Value) + 1);
+        }
+
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            if (animationTimer.IsEnabled)
+            {
+                animationTimer.Stop();
+                startbtn.Text = "Старт";
+            }
+            else
+            {
+                animationTimer.Start();
+                startbtn.Text = "Стоп";
+            }
+        }
+
+        private void resetbtn_Click(object sender, EventArgs e)
+        {
+            isTracking = false;
+            helixViewport.Camera = defaultCamera.Clone();
+            helixViewport.ZoomExtents();
+        }
     }
 
     public class Object
@@ -73,33 +223,57 @@ namespace Space_Flight_Code
         public double Radius;
         public Point3D Center;
         public string texture;
+        public Model3DGroup Model;
+        public Transform3DGroup TransformGroup = new Transform3DGroup();
+        public AxisAngleRotation3D SelfRotation  = new AxisAngleRotation3D();
+        public RotateTransform3D RotationTransform;
+
+        public Object()
+        {
+            RotationTransform = new RotateTransform3D(SelfRotation);
+            TransformGroup.Children.Add(RotationTransform);
+        }
+        public Point3D GetWorldPosition()
+        {
+            return TransformGroup.Transform(Center);
+        }
 
         public Model3DGroup Draw()
         {
+
+            Model = CreateModelGroup();
+            return Model;
+            
+        }
+
+        private Model3DGroup CreateModelGroup()
+        {
             var meshBuilder = new MeshBuilder();
             meshBuilder.AddSphere(Center, Radius, 40, 40);
-        
 
-        var material = new DiffuseMaterial(
-                new ImageBrush
-                {
-                    ImageSource = new BitmapImage(
-                        new Uri(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, texture))),
-                    Stretch = Stretch.Fill
-                }
-            );
-        var sphere = new GeometryModel3D
+
+
+            var material = new DiffuseMaterial(
+                    new ImageBrush
+                    {
+                        ImageSource = new BitmapImage(
+                            new Uri(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, texture))),
+                        Stretch = Stretch.Fill
+                    }
+                );
+            var sphere = new GeometryModel3D
             {
                 Geometry = meshBuilder.ToMesh(),
                 Material = material
             };
 
 
-        var modelGroup = new Model3DGroup();
-        modelGroup.Children.Add(sphere);
-        modelGroup.Children.Add(new AmbientLight(Colors.White));
+            var modelGroup = new Model3DGroup();
+            modelGroup.Children.Add(sphere);
+            modelGroup.Children.Add(new AmbientLight(Colors.White));
+            modelGroup.Transform = TransformGroup;
 
-        return modelGroup;
+            return modelGroup;
         }
     }
 
@@ -110,16 +284,36 @@ namespace Space_Flight_Code
             Radius = 6301f;
             Center = new Point3D(0, 0, 0);
             texture = "textures/earth.jpg";
+            SelfRotation.Axis = new Vector3D(0, 0, 1);
         }
     }
 
     public class Moon : Object
     {
+        private readonly TranslateTransform3D _orbitTransform = new TranslateTransform3D();
+        private readonly RotateTransform3D _orbitRotation = new RotateTransform3D();
+        private double orbitRadius;
+
         public Moon()
         {
             Radius = 1737f;
-            Center = new Point3D(0, 384467f + 6301f, 0);
+            Center = new Point3D(0, 0, 0);
             texture = "textures/moon.jpg";
+            SelfRotation.Axis = new Vector3D(0, 0, 1);
+            orbitRadius = 384467f + 6301f;
+
+            TransformGroup.Children.Insert(0, _orbitRotation);
+            TransformGroup.Children.Insert(0, _orbitTransform);
+
+            UpdateOrbit(0);
+        }
+
+        public void UpdateOrbit(double angle)
+        {
+            _orbitTransform.OffsetX = orbitRadius * Math.Cos(angle * Math.PI / 180);
+            _orbitTransform.OffsetY = orbitRadius * Math.Sin(angle * Math.PI / 180);
+
+            _orbitRotation.Rotation = new AxisAngleRotation3D(new Vector3D(0, 0, 1), angle);
         }
     }
 }
